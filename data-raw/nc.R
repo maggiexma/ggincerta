7fd52ceda4597db9345e351d0d25e8538fea6136
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-10-04 16:37:37 +1000

    update nc data so sd is not negative

--- data-raw/nc.R
+++ data-raw/nc.R
@@ -6,7 +6,7 @@ set.seed(10086)
 nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE) %>%
   mutate(
     value = rnorm(n()),
-    sd = rnorm(n())
+    sd = runif(n(), 0.5, 4)
   )
 
 usethis::use_data(nc, internal = FALSE, overwrite = TRUE)
