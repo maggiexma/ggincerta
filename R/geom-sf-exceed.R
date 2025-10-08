8bac949ed0513536e8d0d1888c7515f87d8a8989
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-10-09 10:54:56 +1100

    delete dist_unif

--- R/geom-sf-exceed.R
+++ R/geom-sf-exceed.R
@@ -100,10 +100,3 @@ dist_norm <- function(x, v1, v2) {
   stats::pnorm(x, mean = v1, sd = v2, lower.tail = FALSE)
 }
 
-#' @export
-dist_unif <- function(x, v1, v2) {
-  stats::punif(x,
-               min = min(v1, na.rm = TRUE),
-               max = max(v1, na.rm = TRUE),
-               lower.tail = FALSE)
-}
