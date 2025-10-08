61015c4a4a2c7a1f70e4456146f10210753b5f47
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-10-09 10:54:48 +1100

    suggested duo exceed

--- R/duo.R
+++ R/duo.R
@@ -9,3 +9,26 @@ duo <- function(v1, v2) {
     vars = c(.v1, .v2)
   )
 }
+
+duo_exceed <- function(estimate, error, dist_fun, threshold) {
+  .v1 = substitute(estimate)
+  .v2 = substitute(error)
+  ind <- seq_along(estimate)
+  structure(
+    lapply(ind, function(i) list(v1 = v1[i], v2 = v2[i])),
+    class = c("exceed", "list"),
+    dist_fun = dist_fun,
+    threshold = threshold,
+    vars = c(.v1, .v2)
+  )
+}
+
+exceed <- function(..., dist_fun, threshold = NA) {
+  structure(list(args = list(...)),
+            threshold = threshold,
+            dist_fun = dist_fun,
+            class = c("exceed", "list"))
+
+  dist_fun(!!args)
+
+}
