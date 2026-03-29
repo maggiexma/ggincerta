57035891ce4021c831a64a14a58328b58915e2f6
maggiema <maggiema1207@163.com>
2026-03-29 16:56:19 +1100

    add simulated data for transformation testing

--- data-raw/nc.R
+++ data-raw/nc.R
@@ -4,9 +4,12 @@ library(dplyr)
 set.seed(10086)
 
 nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE) |>
-  mutate(
-    value = rnorm(n()),
-    sd = runif(n(), 0.5, 4)
+  dplyr::mutate(
+    value = rnorm(dplyr::n()),
+    sd = runif(dplyr::n(), 0.5, 4),
+
+    value_log10 = 10^runif(dplyr::n(), -1, 2),
+    sd_log2 = 2^runif(dplyr::n(), -2, 3)
   )
 
 usethis::use_data(nc, internal = FALSE, overwrite = TRUE)
